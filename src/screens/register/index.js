3723377f31f57fa05752530eb85b3f59a788b0c1
Christopher Daniel Ramirez Flores <ramozchris@gmail.com>
2024-08-12 23:37:31 -0600

    styles: add RegisterScreen styles
    New paths
    Add Style file to RegisterScreen
    Add screen to router

--- src/screens/register/index.js
+++ src/screens/register/index.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { Link, useNavigate } from 'react-router-dom';
-import { useUser } from '../contexts/UserContext';
+import { useUser } from '../../contexts/UserContext';
+
+// Styles
+import './styles.sass';
+
+// FA
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faLock, faEnvelope, fa0 } from '@fortawesome/free-solid-svg-icons'; 
 
 function Register({ onRegisterSuccess }) {
   const [name, setName] = useState('');
@@ -51,48 +58,53 @@ function Register({ onRegisterSuccess }) {
   };
 
   return (
-    <div>
+    <div className='RegisterScreen'>
       <form onSubmit={handleRegister}>
-        <h2>Register</h2>
-        <div>
-          <label>Name:</label>
+        <h2>Registrarse</h2>
+        <p>Registrate para acceder a los servicios de Coding Coach</p>
+        <div className='Input'>
+          <label><FontAwesomeIcon icon={faUser} /></label>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            placeholder='Nombre'
             required
           />
         </div>
-        <div>
-          <label>Email:</label>
+        <div className='Input'> 
+          <label><FontAwesomeIcon icon={faEnvelope} /></label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder='Email'
             required
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className='Input'>
+          <label><FontAwesomeIcon icon={faLock} /></label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder='Contraseña'
             required
           />
         </div>
-        <div>
-          <label>Age:</label>
+        <div className='Input'>
+          <label><FontAwesomeIcon icon={fa0} /></label>
           <input
             type="number"
             value={age}
             onChange={(e) => setAge(e.target.value)}
+            placeholder='Edad'
             required
           />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit">Continuar</button>
       </form>
-      <p>Ya tienes una cuenta? <Link to="/">Iniciar sesión</Link></p>
+      <p>Ya tienes una cuenta? <Link to="/login">Iniciar sesión</Link></p>
     </div>
   );
 }
